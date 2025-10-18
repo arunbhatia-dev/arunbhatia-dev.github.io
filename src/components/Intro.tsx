44b26161e735d439ab8388d7582849b3d9dc920c
Arun <arun.bhatia@aalto.fi>
2025-10-18 13:09:11 +0300

    Added 3d laptop

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,6 @@
 import React  from "react";
 import Typist from "react-typist-component";
+import Laptop from "./Laptop";
 
 import "../styles/Intro.css";
 
@@ -8,20 +9,23 @@ class Intro extends React.Component {
     render() {
         return (
             <div id="intro">
+              <div className="laptop-container">
+                <Laptop />
+              </div>
               <div id="intro-desc">
                 <div className="intro-greeting">
                   <Typist
                     typingDelay={100}
                     cursor={<span className="cursor">|</span>}
                   >
-                    hi, 
+                    hi,
                     <span className="intro-name"> arun </span>
                     here.
                   </Typist>
                 </div>
                 <div className="intro-description">
                   <p className="fade-in-text">
-                  I'm a software engineer from Helsinki, Finland. 
+                  I'm a software engineer from Helsinki, Finland.
                   I'm fascinated by high-impact products and technologies that make a difference.
                   </p>
                 </div>
